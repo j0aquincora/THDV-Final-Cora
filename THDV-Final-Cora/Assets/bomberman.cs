0ba94a12db766da9f029f54562e5b05ab1ceac4b
joaquin <joaquin.cora@estudiante.imagecampus.edu.ar>
2024-12-02 20:29:23 -0300

    bomba con explosion

--- THDV-Final-Cora/Assets/bomberman.cs
+++ THDV-Final-Cora/Assets/bomberman.cs
@@ -1,23 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class bomberman : MonoBehaviour
 {
+    public GameObject bomba;
+    public int bombAmmo;
+    public Transform playerPos;
+    public TMP_Text bombAmmoText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateBombAmmoUI();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("bomb"))
+        {
+            bombAmmo++;
+            Destroy(collision.gameObject);
+            UpdateBombAmmoUI();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PlaceBomb();
+        }
+
     }
     void PlaceBomb()
     {
-
+        bombAmmo--;
+        Instantiate(bomba,playerPos.position,playerPos.rotation);
+        UpdateBombAmmoUI();
     }
-
+    void UpdateBombAmmoUI()
+    {
+        bombAmmoText.text = "Bombas: " + bombAmmo.ToString(); // Actualiza el texto con el valor de bombAmmo
+    }
 }
