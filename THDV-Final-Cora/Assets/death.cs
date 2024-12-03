9d92b0f3403e24b39d4908d8efe480ffdb0e6f9c
joaquin <joaquin.cora@estudiante.imagecampus.edu.ar>
2024-12-02 21:22:48 -0300

    Obstaculos rotativos

--- THDV-Final-Cora/Assets/death.cs
+++ THDV-Final-Cora/Assets/death.cs
@@ -2,7 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement; 
 public class death : MonoBehaviour
 {
-    
+    public float rotationSpeed;
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         
