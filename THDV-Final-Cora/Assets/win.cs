0375616a9929e475066c8b6163cdaf8f59c60080
joaquin <joaquin.cora@estudiante.imagecampus.edu.ar>
2024-12-02 21:22:48 -0300

    Obstaculos rotativos

--- THDV-Final-Cora/Assets/win.cs
+++ THDV-Final-Cora/Assets/win.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class win : MonoBehaviour
 {
+    public GameObject winText;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,14 @@ public class win : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            winText.SetActive(true);
+            
+        }
     }
 }
